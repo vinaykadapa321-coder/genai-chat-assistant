287feb8b9f08e5f30e13f0e9aea81fbb948acc86
vinay <vinaykadapa321@gmail.com>
2026-03-08 17:26:28 +0530

    fixed render port

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ def chat():
         if user_message in doc["title"].lower():
             return jsonify({"reply": doc["content"]})
     return jsonify({"reply": "Sorry, I don't know the answer."})
-
+import os
 if __name__=="__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0", port=port)
