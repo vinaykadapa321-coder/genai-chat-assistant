051128a565d17c23c7a3c5939c1a6bea500979e1
vinay <vinaykadapa321@gmail.com>
2026-06-12 11:15:56 +0530

    Clean repository

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 from flask import Flask, render_template, request, jsonify
-import json 
+import google.generativeai as genai
+import os
 
 app = Flask(__name__)
 
-with open("docs.json") as f:
-    documents = json.load(f)
+# Paste your Gemini API Key here
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+
+model = genai.GenerativeModel("gemini-2.5-flash")
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
 @app.route("/chat", methods=["POST"])
 def chat():
-    user_message= request.json["message"].lower()
+    user_message = request.json["message"]
 
-    for doc in documents:
-        if doc["title"].lower() in user_message:
-            return jsonify({"reply": doc["content"]})
-    return jsonify({"reply": "Sorry, I don't know the answer."})
-import os
-if __name__=="__main__":
-    port = int(os.environ.get("PORT",5000))
+    response = model.generate_content(user_message)
+
+    return jsonify({
+        "reply": response.text
+    })
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
